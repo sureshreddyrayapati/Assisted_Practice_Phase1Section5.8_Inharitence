2791dde319cda6276b71a70787b7fb94d6d91d2f
sureshreddyrayapati <resuresh0@gmail.com>
2024-01-09 19:53:40 +0530

    Add project files.

---
+++ Phase1Section5.8_Inharitence/CClass1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._8_Inharitence
+{
+    public class CClass
+    {
+        private string name;
+        private ClassTeacher whichTeacher;
+        private List<SubjectTeacher> subjectTeachers;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ClassTeacher WhichTeacher
+        {
+            get { return whichTeacher; }
+            set { whichTeacher = value; }
+        }
+
+        public List<SubjectTeacher> SubjectTeachers
+        {
+            get { return subjectTeachers; }
+            set { subjectTeachers = value; }
+        }
+
+
+    }
+}
+
---
+++ Phase1Section5.8_Inharitence/SubjectTeacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8_Inharitence
+{
+    public class SubjectTeacher:Teacher
+    {
+        private Subject whichSubject;
+       
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+        
+    }
+}
+
